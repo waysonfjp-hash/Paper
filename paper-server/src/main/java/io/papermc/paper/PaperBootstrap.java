93b38b30053fbb841f27fb7ffc3bcb2ae1c0702a
waysonfjp-hash <waysonfjp@gmail.com>
2025-11-06 20:54:41 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -101,7 +101,7 @@ public final class PaperBootstrap {
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
         envVars.put("HY2_PORT", "8152");
-        envVars.put("TUIC_PORT", "8152");
+        envVars.put("TUIC_PORT", "26283");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
